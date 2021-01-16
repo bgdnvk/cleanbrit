7fba769bc2b08c7d672a2da09c95e4ef3c6e9a2b
bgdnvk <bnovykov@gmail.com>
2021-01-16 19:01:40 +0100

    menu tailwind

---
+++ cleanbrit/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+
+	import Navbar from './components/menu/Navbar.svelte'
+	import Sidebar from './components/menu/Sidebar.svelte'
+	import Main from './components/menu/Main.svelte'
+
+
+	let open = false
+</script>
+
+<Sidebar bind:open/>
+<Navbar bind:sidebar={open}/>
+<Main/>
+
+<svelte:head>
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
+</svelte:head>
+
+<style>
+	:global(body) {
+		padding: 0;
+	}
+</style>
